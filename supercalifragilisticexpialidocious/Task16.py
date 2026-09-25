7d9f842937db5986ed9ea2ceae4bb3b0bb633501
JohnPork696 <24VASILIAUSKASEv@hazelwick.org>
2026-09-25 13:57:57 +0000

    25/09/2026

---
+++ supercalifragilisticexpialidocious/Task16.py
@@ -0,0 +1,7 @@
+length = input("Enter the base length: ")
+length2 = int(length)
+height = input("Enter the height: ")
+height2 = int(height)
+
+calc1 = length2 * height2
+print(f"The area of the triangle is: {calc1 / 2}")
